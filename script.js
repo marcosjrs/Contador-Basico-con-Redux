87b562724414c78b140746ecb825949772e66826
marcosjrs <marcosjrs80@gmail.com>
2017-11-12 11:17:20 +0100

    Definición de los "actores" mínimos de redux

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
+//State
+var estado = {
+    cantidad: 0
+};
+
+//Reducer
+function reductor(estado, accion){
+}
+
+//Store. Através del store se despacharán acciones. ("dispatcharán" eventos :D )
+var store = Redux.createStore(reductor, estado);
+
+//Action
+var accionAumentar = {
+    type:"aumentar"
+}
+
+var accionDisminuir = {
+    type:"disminuir"
+}
 
 
